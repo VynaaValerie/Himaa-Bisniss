5db1b69a37c73c704680a3b71005249b412e8d09
Vynaa <118973846+VynaaValerie@users.noreply.github.com>
2025-11-02 02:05:23 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html for all routes (SPA)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`HIMABIS Website running on port ${PORT}`);
+  console.log(`Visit: http://localhost:${PORT}`);
+});
+
+module.exports = app;
